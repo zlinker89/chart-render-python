2647cd24f171731baf353b8fe95c0f6db6988ae1
Franklin ospino <desarrollo@expertosip.com>
2023-03-16 15:05:38 -0500

    retiramos el certificado

--- app.py
+++ app.py
@@ -2,6 +2,4 @@ import uvicorn
 
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=False, access_log=True, 
-        ssl_keyfile='key.pem',
-        ssl_certfile='cert.pem')
+    uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=False, access_log=True)
