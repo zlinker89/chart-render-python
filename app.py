8e1d72104ee48307a0d70aa4cfccadd6147a8c8d
Franklin ospino <desarrollo@expertosip.com>
2023-03-16 14:38:44 -0500

    certificado ssl

--- app.py
+++ app.py
@@ -2,4 +2,6 @@ import uvicorn
 
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=False, access_log=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=False, access_log=True, 
+        ssl_keyfile='key.pem',
+        ssl_certfile='cert.pem')
